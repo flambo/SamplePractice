76b9fe69c4789232d0a517126b72f4ed920bd96c
flambo <ieem52057@gmail.com>
2017-04-15 18:13:58 +0900

    Commit 3

--- Sample.java
+++ Sample.java
@@ -3,5 +3,6 @@ public class Sample{
   public static void main(String[] args){
     System.out.println("Sample Test");
     System.out.println("Sample Test2");
+      System.out.println("Sample Test3");
   }
 }
